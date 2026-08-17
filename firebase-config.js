63961bf7035f43df410d1472afd3e16815a27805
kirche-heli <kirche.heli@gmail.com>
2026-08-17 21:16:57 +0200

    Neue Adventskalender-Seite: Startseite, Verwaltung, Impressum, Datenschutz

---
+++ firebase-config.js
@@ -0,0 +1,23 @@
+/* ------------------------------------------------------------------
+   Firebase-Zugangsdaten - ECHTBETRIEB
+
+   Die Seite arbeitet mit der Datenbank des Projekts
+   "adventskalender-90bb1".
+
+   Zum Zurueckschalten in den Vorschau-Modus (Daten nur im Browser):
+   den Block unten auskommentieren und stattdessen
+   window.FIREBASE_CONFIG = null; setzen.
+
+   Diese Zugangsdaten sind kein Passwort und duerfen oeffentlich im
+   Quelltext stehen - der Schutz kommt allein durch die
+   Sicherheitsregeln (firestore.rules).
+------------------------------------------------------------------- */
+
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyCZuvHUy6xMN6WIrHUQBpxuobZkQRKB20s",
+  authDomain: "adventskalender-90bb1.firebaseapp.com",
+  projectId: "adventskalender-90bb1",
+  storageBucket: "adventskalender-90bb1.firebasestorage.app",
+  messagingSenderId: "816945632051",
+  appId: "1:816945632051:web:817e7a3d8945c3a65aa446"
+};
